89c66f04ee792c81bbad02697f2f260f660c353c
Kairat Kurmanaliev <kurmanaliev2403@gmail.com>
2020-08-25 18:13:25 +0600

    first_push

---
+++ src/bffbook/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def home_view(request):
+
+    return render(request,'main/home.html',{})
---
+++ src/profiles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Profile
+# Create your views here.
+
+def my_profile(request):
+    profile = Profile.objects.get(user = request.user)
+
+    return render(request,'profiles/myprofile.html',{'profile':profile})
