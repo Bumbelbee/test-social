4944bdeff5bf823273ac68fe81cfe88032dfb8af
Kairat Kurmanaliev <kurmanaliev2403@gmail.com>
2020-09-06 16:03:05 +0600

    lesson 10

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -3,5 +3,9 @@ from .views import *
 app_name = 'profiles'
 
 urlpatterns = [
-    path('myprofile',my_profile)
+    path('myprofile',my_profile,name="my-profile"),
+    path('my-invites',invites_received_view,name="my-invites-view"),
+    path('all-profiles-list',profiles_list,name="all-profiles-list"),
+    path('invites-profiles-list',invites_profiles_list,name="invites-profiles-list"),
+
 ]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile,Relationship
 from .forms import ProfileModelForm
 # Create your views here.
 
@@ -20,3 +20,32 @@ def my_profile(request):
     return render(request,'profiles/myprofile.html',context)
 
 
+def invites_received_view(request):
+    profile = Profile.objects.get(user = request.user)
+    qs = Relationship.objects.invantions_received(profile)
+
+    context = {
+        'qs':qs,
+    }
+
+    return render(request,'profiles/my_invites.html',context)
+
+def profiles_list(request):
+    user = request.user
+    qs = Profile.objects.get_all_profiles(user)
+
+    context = {
+        'qs':qs,
+    }
+
+    return render(request,'profiles/profiles_list.html',context)
+
+def invites_profiles_list(request):
+    user = request.user
+    qs = Profile.objects.get_all_profiles_to_invites(user)
+
+    context = {
+        'qs':qs,
+    }
+
+    return render(request,'profiles/invites_profiles_list.html',context)
